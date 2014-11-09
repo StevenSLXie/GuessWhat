70c3231e4f91fceb625c8a2a0fdd8b32b2dde746
StevenSLXie <stevenslxie@gmail.com>
2014-11-09 11:20:23 +0800

    fixed the 1% bug

--- Guess/algorithm/data_processing.py
+++ Guess/algorithm/data_processing.py
@@ -3,6 +3,6 @@ from Guess.models import Game
 
 def price_change(primary_key):
 	game = Game.objects.get(pk=primary_key)
-	game.price_home = game.num_home/(game.num_home+game.num_away+0.1)*100
-	game.price_away = 100-game.price_home
+	game.price_home = game.num_home/(game.num_home+game.num_away+0.0001)*100
+	game.price_away = 100-int(game.price_home)
 	game.save()
