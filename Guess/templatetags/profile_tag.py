73ac6ea0284aec32bb64e0b7f987df5ab6ff9bef
StevenSLXie <stevenslxie@gmail.com>
2014-11-17 21:18:27 +0800

    changed to the 2 col style

--- Guess/templatetags/profile_tag.py
+++ Guess/templatetags/profile_tag.py
@@ -4,4 +4,8 @@ register = template.Library()
 
 @register.simple_tag()
 def sub(value1, value2):
-    return value1-value2
+	return value1-value2
+
+@register.simple_tag()
+def mod(value1, value2):
+	return value1%value2
