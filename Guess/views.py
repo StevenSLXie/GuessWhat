418dc93af5fba13b77eb704b8a2672fde9a25fb9
StevenSLXie <stevenslxie@gmail.com>
2014-11-26 20:36:37 +0800

    used messages|length.

--- Guess/views.py
+++ Guess/views.py
@@ -253,7 +253,6 @@ def proposal(request):
 
 def encap_para(para,person):
 	messages = find_unread(person)
-	para['unread'] = len(messages)
 	para['messages'] = messages
 	para['person'] = person
 	return para
